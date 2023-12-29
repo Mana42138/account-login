9e058a3f30c6bd6876627688315d0b8212145135
Mana42138 <86923626+Mana42138@users.noreply.github.com>
2023-12-30 00:50:43 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,16 @@
-from register import main
+from register import main, readfile
+from auto_copy import main_copy
+import threading
+
+data = readfile("settings.json")
+
+auto_copy = data["AUTO_COPY_CODES"]
+
+if auto_copy:
+    thread_one = threading.Thread(target=main_copy)
+    thread_one.start()
 
 while True:
-    main()
+    thread_two = threading.Thread(target=main)
+    thread_two.start()
+    thread_two.join()
