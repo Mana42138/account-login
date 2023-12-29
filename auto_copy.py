dd71ecf369bf55a399bf498b03b9ffc52c784066
Mana42138 <86923626+Mana42138@users.noreply.github.com>
2023-12-30 00:49:29 +0100

    Create auto_copy.py

---
+++ auto_copy.py
@@ -0,0 +1,33 @@
+import keyboard
+import pyperclip
+
+
+word_list = [
+"UPD4",
+"SORRYDELAYS3",
+"100MVISITSTHANKS",
+"SEASONONE",
+"SORRYDELAYS2",
+"BETA",
+"XMAS",
+"FOLLOWER",
+]
+
+index = 0
+
+def copy_next_word(event):
+    global index
+    if event.name == 'f':  # you can change 'f' to any key
+        if index < len(word_list):
+            word_to_copy = word_list[index]
+            index += 1
+            pyperclip.copy(word_to_copy)
+            # print(f"Copied: {word_to_copy}")
+        else:
+            index = 0
+            # print("End of the list. Restarting...")
+
+def main_copy():
+    keyboard.on_press(copy_next_word)
+
+    keyboard.wait('esc')
