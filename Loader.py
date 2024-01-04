839e9dfb2fdb8bdd14723c90a8b0a8f059e8d35c
Mana42138 <86923626+Mana42138@users.noreply.github.com>
2024-01-04 18:16:33 +0100

    Create Loader.py

---
+++ Loader.py
@@ -0,0 +1,23 @@
+import sys
+import subprocess
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'requests'])
+
+import requests
+import os
+import sys
+
+files_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+file_name = "setup.py"
+Item_Path = os.path.join(files_path, file_name)
+
+if os.path.exists(Item_Path):
+    os.remove(Item_Path)
+
+print("Installing Setup / requirements.txt")
+with open(os.path.join(files_path, file_name), "wb") as file:
+    file.write(requests.get("https://raw.githubusercontent.com/Mana42138/account-login/main/setup.py").content)
+
+with open(os.path.join(files_path, "requirements.txt"), "wb") as file:
+    file.write(requests.get("https://raw.githubusercontent.com/Mana42138/account-login/main/requirements.txt").content)
